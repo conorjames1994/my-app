7c9f2e74aa2bf413a5f1051626f875ede7bfdf1d
conorjames1994 <161840402+conorjames1994@users.noreply.github.com>
2025-01-04 12:34:34 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({path: './config/config.env'});
 
 const connectDB = async () => {
 try{
-  const conn = await mongoose.connect("mongodb+srv://conor.james%40nhs.net:Afc123cbj@cluster0.7siir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  const conn = await mongoose.connect("mongodb+srv://conorjames:expensestrackerj@cluster0.7siir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
   console.log(`Mongo db connected ${conn.connection.host}`.cyan.underline.bold)
 }
